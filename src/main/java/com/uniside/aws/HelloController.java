920f4f21b999d6ca3b8852bde832fea95772a59e
feelgoodn <nikhilkotale2000iit@gmail.com>
2024-05-23 10:55:12 +0530

    initial commit

---
+++ src/main/java/com/uniside/aws/HelloController.java
@@ -0,0 +1,17 @@
+package com.uniside.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController 
+{
+	 @GetMapping
+	    public String hello() {
+	        return "Hello! Welcome to UniSide";
+	    }
+	}
+
+
